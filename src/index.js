741b5b7cdc0242b7db7a7fb290c51bc539e21c9e
Alexander Cuenca <cuencaalexander@gmail.com>
2019-08-06 23:29:12 -0700

    End of Section 7

--- src/index.js
+++ src/index.js
@@ -1,28 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app/layout/App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./app/layout/App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+import { configureStore } from "./app/store/configureStore";
+import { Provider } from 'react-redux'
 
-const rootEl = document.getElementById('root')
+const store = configureStore()
+console.log(store.getState())
+
+const rootEl = document.getElementById("root");
 
 let render = () => {
-    ReactDOM.render(
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>,
-        rootEl
-    );
-}
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+    rootEl
+  );
+};
 
 if (module.hot) {
-    module.hot.accept('./app/layout/App', () => {
-        setTimeout(render);
-    })
+  module.hot.accept("./app/layout/App", () => {
+    setTimeout(render);
+  });
 }
 
-render()
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
