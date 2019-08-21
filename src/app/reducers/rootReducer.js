47f889dee4c833fb25c2d138de6d44218bf6e1bd
Alexander Cuenca <cuencaalexander@gmail.com>
2019-08-21 13:12:49 -0700

    End of Secion 9

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import {reducer as FormReducer} from 'redux-form'
 import testReducer from "../../features/testarea/testReducer";
 import eventsReducer from "../../features/event/eventsReducer";
 
 const rootReducer = combineReducers({
+    form: FormReducer,
     test: testReducer,
     events: eventsReducer
 })
